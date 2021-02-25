7a4cc254f08d5e435b0aa1d7af08ebc749ac12f0
gabrieldeml <gabrielddeml@gmail.com>
2021-02-25 00:15:43 -0500

    runNeuralNetwork doesn't work yet

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,40 +5,29 @@ import * as tf from '@tensorflow/tfjs'
 
 import { Button } from '@material-ui/core';
 
-const model = tf.sequential();
-model.add(tf.layers.dense({units: 1, inputShape: [200]}));
-model.compile({
-    loss: 'meanSquaredError',
-    optimizer: 'sgd',
-    metrics: ['MAE']
-});
 
+async function runNeuralNetwork() {
+    const mobilenet = require('@tensorflow-models/mobilenet');
 
-// Generate some random fake data for demo purpose.
-const xs = tf.randomUniform([10000, 200]);
-const ys = tf.randomUniform([10000, 1]);
-const valXs = tf.randomUniform([1000, 200]);
-const valYs = tf.randomUniform([1000, 1]);
+    const img = document.getElementById('img');
 
- 
-// Start model training process.
-async function train() {
-    await model.fit(xs, ys, {
-        epochs: 100,
-        validationData: [valXs, valYs],
-        // Add the tensorBoard callback here.
-        // callbacks: tf.node.tensorBoard('/tmp/fit_logs_1')
-    });
+    // Load the model.
+    const model = await mobilenet.load();
+
+    // Classify the image.
+    const predictions = await model.classify(img);
+
+    console.log('Predictions: ');
+    console.log(predictions);
 }
-
-
-
 
 function Home() {
     return (
         <div className="App">
             <header className="App-header">
                 <h1>Main page</h1>
+                <img ref={"img"}></img>
+                <Button variant="contained" onClick={() => { alert('clicked') }}>Run Neural </Button>
             </header>
         </div>
     );
