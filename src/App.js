d22b8026658c6e85ebfa67a41766cf1161ec3718
gabrieldeml <gabrielddeml@gmail.com>
2021-02-26 12:44:36 -0500

    ML stuff is working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import React from 'react';
 import logo from './logo.svg';
+import cat from './cat.jpeg';
 import './App.css';
 
+
+import * as tf from '@tensorflow/tfjs';
+
+async function testML(imgRef) {
+
+
+  const mobilenet = require('@tensorflow-models/mobilenet');
+
+  const img = imgRef.current;
+
+  // Load the model.
+  const model = await mobilenet.load();
+
+  // Classify the image.
+  const predictions = await model.classify(img);
+
+  console.log('Predictions: ');
+  console.log(predictions);
+}
+
 function App() {
+  const imgRef = React.createRef();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={cat} ref={imgRef} />
+        <button onClick={() => testML(imgRef)}>Press me</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
