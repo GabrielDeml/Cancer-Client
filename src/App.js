09d7e2d941cc23b82262801cf7ae2e7109f7b015
gabrieldeml <gabrielddeml@gmail.com>
2021-02-27 14:42:06 -0500

    Updated comments

--- src/App.js
+++ src/App.js
@@ -24,31 +24,23 @@ class App extends Component {
     this.setState({ msg: '' })
   }
 
-  // TODO: Figure out why this isn't being deffined
   async testML(imgRef) {
-
-
-    const mobilenet = require('@tensorflow-models/mobilenet');
-
+    // const mobilenet = require('@tensorflow-models/mobilenet');
+    // Get the image reference
     const img = tf.browser.fromPixels(imgRef.current);
-
+    // Convert the image to a 4d tensor
     const smallImg = tf.image.resizeBilinear(img, [224, 224]);
     const resized = tf.cast(smallImg, 'float32');
     const t4d = tf.tensor4d(Array.from(resized.dataSync()), [1, 224, 224, 3])
-    // img.reshape(1, 224, 224, 3)
-
     // Load the model.
-    // const model = await mobilenet.load();
     const model = await tf.loadLayersModel('https://gabrieldeml.github.io/Cancer-Client/models/model.json');
-
-
     // Classify the image.
     const predictions = model.predict(t4d);
-
+    // Get the output
     const value = predictions.dataSync()
-    // TODO show user the output of the predcition
     console.log('Predictions: ');
     console.log(value);
+    // Display the output of the classification
     if (value[0] == 0) {
       this.setState({ msg: 'malignant' })
     } else {
@@ -73,7 +65,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>This isn't medical advice it just is a test</h1>
+          <h1>This is not medical advice it just is a test</h1>
           <input type="file" accept="image/*" onChange={handleImageUpload} ref={imgRef} multiple="false" />
           <img src={cat} ref={imgRef} />
           <button onClick={() => this.testML(imgRef)}>Run model</button>
